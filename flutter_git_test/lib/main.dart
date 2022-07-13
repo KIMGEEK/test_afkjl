6886e2b1948a4ef418c7f82fd5cfc527e2f8e3a8
LAPTOP-MLL80S8J\kgeek <baekhajun@chungbuk.ac.kr>
2022-07-13 16:51:13 +0900

    Modified main.dart

--- flutter_git_test/lib/main.dart
+++ flutter_git_test/lib/main.dart
@@ -13,18 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Hello'),
     );
   }
 }
@@ -48,7 +39,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 3;
 
   void _incrementCounter() {
     setState(() {
